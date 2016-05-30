faee136b0cf0382487bac8d8cb3ffa0051ec0123
padimkj <padimkj@gmail.com>
2016-05-30 01:09:41 -0700

    history.html now displays a user's wager. Still need tests for it. Also, figure out way to simulate being logged in...?

--- betonreddit/history/views.py
+++ betonreddit/history/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 
+
+from wager.models import Wager, Betline
+
 # Create your views here.
 
 
 def history_main(request):
+
+    try:
+        
+        if request.user.is_authenticated():
+            print("AUTHENTICATO X")
+        else:
+            print("NOT AUTHENTICATED")
+        
+    except:
+        print("something went wrong. except.")
+        pass
+
+        
+    context_dict = {}
     
-    return render(request, "betonreddit/history.html", {})
+    all_wagers = Wager.objects.all()
+    
+    context_dict['all_wagers'] = Wager.objects.all()
+    
+    
+    #Once we can authenticate a user, we can fetch their wagers.
+    #context_dict['all_wagers'] = Wager.objects.filter(email=request.user.email)
+    
+    return render(request, "betonreddit/history.html", context_dict)
+    
+    
+
+
